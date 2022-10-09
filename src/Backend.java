bc6a99ce4071462f31fe0a5a56828f97369472f6
Abhinav Srivastava <atg271@gmail.com>
2022-10-09 19:53:29 -0400

    wip

--- src/Backend.java
+++ src/Backend.java
@@ -1,3 +1,14 @@
+/*
+This code is a direct fork of the code provided in A1, with tweaks for readability.
+Cell has been replaced with Card - and a few member states have been modified.
+Board has been replaced with Deck - and a few member states have been modified.
+Player and Banker have been separated into their own classes - for ease of use.
+
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Backend {
 
     int numberOfPlayers;
@@ -6,78 +17,101 @@ public class Backend {
     int[][] scoreBoard; // dict
     int[] draws;
 
-    enum GameName {
-        TICTACTOE, ORDERANDCHAOS, TRIANTAENA
-    }
-
-    enum CellState {
-        EMPTY, X, O
-    }
-
-    interface ruleSet {
-        boolean isValidMove(Cell move, int boardSize);
-        boolean ifPlayerWon(Cell move, Board board, int boardSize);
-    }
-
-    static class Cell {
-        int x_coord;
-        int y_coord;
-        public CellState state;
-        Player player;
-
-        public Cell(int x, int y, CellState state) {
-            this.x_coord = x;
-            this.y_coord = y;
-            this.state = state;
-
-        }
-    }
-
-    class Board {
-        public int boardSize;
-        public Cell[][] grid = new Cell[boardSize][boardSize];
-
-        public Board(int boardSize) {
-            this.boardSize = boardSize;
-            grid = new Cell[boardSize][boardSize];
-            for (int i = 0; i < boardSize; i++) {
-                for (int j = 0; j < boardSize; j++) {
-                    grid[i][j] = new Cell(i, j, CellState.EMPTY);
-                }
-            }
-        }
-    }
-
-    static class Player {
-        int id;
-        String name;
-        int score;
-        boolean isHuman;
-        boolean isTurn;
-        int[] draw;
-        CellState assignedMove;
-        boolean isWinner = false;
-
-        public Player(int id, String name) {
-            this.id = id;
-            this.name = name;
-            this.isHuman = false;
-            this.score = 0;
-            this.isTurn = false;
-        }
-    }
-
-    class Team {
-        int numPlayers;
-        Player[] players;
-    }
-
     public Backend(int numberOfPlayers, int numberOfRounds) {
         this.numberOfPlayers = numberOfPlayers;
         this.numberOfRounds = numberOfRounds;
         this.players = new Player[numberOfPlayers];
         this.scoreBoard = new int[numberOfPlayers][numberOfRounds];
         this.draws = new int[numberOfRounds];
+    }
+
+    enum GameName {
+        TICTACTOE, ORDERANDCHAOS, TRIANTAENA
+    }
+
+    interface ruleSet {
+    }
+
+    static class Player {
+        int id;
+        String name;
+        int score;
+        boolean isTurn;
+        int[] balance;
+        boolean isWinner = false;
+
+        public Player(int id, String name) {
+            this.id = id;
+            this.name = name;
+            this.score = 0;
+            this.isTurn = false;
+        }
+    }
+
+    // create a card class
+    public class Card {
+        private final String suit;
+        private final int value;
+
+        public Card(String suit, int value) {
+            this.suit = suit;
+            this.value = value;
+        }
+
+        public String getSuit() {
+            return suit;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public String toString() {
+            return value + " of " + suit;
+        }
+    }
+
+    public class Deck {
+        private final ArrayList<Card> cards;
+        private int size;
+
+        public Deck() {
+            cards = new ArrayList<Card>();
+            size = 0;
+        }
+
+        public void addCard(Card card) {
+            cards.add(card);
+            size++;
+        }
+
+        public void removeCard(Card card) {
+            cards.remove(card);
+            size--;
+        }
+
+        public Card getCard(int index) {
+            return cards.get(index);
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public void shuffle() {
+            Collections.shuffle(cards);
+        }
+
+        public void printDeck() {
+            for (int i = 0; i < size; i++) {
+                System.out.println(cards.get(i));
+            }
+        }
+    }
+
+    class Team {
+        int numPlayers;
+        Player[] players;
     }
 
 }
